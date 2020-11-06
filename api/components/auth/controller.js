4bbb76591ac1e8197f9ee3f8a2c0d5270903190b
Brahianf <Brahianf@unicauca.edu.co>
2020-11-06 18:44:36 -0500

    bcrypt

--- api/components/auth/controller.js
+++ api/components/auth/controller.js
@@ -1,5 +1,6 @@
 const TABLE = 'auth'
 const auth = require('../../../auth')
+const bcrypt = require('bcrypt')
 
 module.exports = function(injectedStore){
   let store = injectedStore;
@@ -10,15 +11,19 @@ module.exports = function(injectedStore){
 
   async function login(username, password){
     const data = await store.query(TABLE, { username: username });
-    if(data.password === password){
-      //Generar Token
-      return auth.sign(data);
-    } else {
-      throw new Error('Información Inválida')
-    }
+
+    bcrypt.compare(password, data.password)
+      .then(sonIguales => {
+        if (sonIguales === true){
+          //Generar Token
+          return auth.sign(data);
+        } else {
+          throw new Error('Información Inválida')
+        }
+      });
   }
 
-  function upsert(data){
+  async function upsert(data){
     const authData = {
       id: data.id,
     }
@@ -28,7 +33,7 @@ module.exports = function(injectedStore){
     }
 
     if (data.password) {
-      authData.password = data.password;
+      authData.password = await bcrypt.hash(data.password, 5);
     }
 
     return store.upsert(TABLE, authData);
